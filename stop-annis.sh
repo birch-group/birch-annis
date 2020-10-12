8ac35406cbc15997a623960a7e86357d82c7decc
parkervg <parkervg5@gmail.com>
2020-10-12 09:34:50 -0700

    Helpful echoes

--- stop-annis.sh
+++ stop-annis.sh
@@ -4,5 +4,7 @@ source ./bin/config.sh
 export ANNIS_HOME=./annis-service-3.6.0
 export PATH=$PATH:$ANNIS_HOME/bin
 
+echo Stopping Annis...
 annis-service.sh stop
+echo Shutting down Tomcat....
 sudo systemctl stop tomcat
