eb80f9ee60a5962080a8488ff66b11eb6f835f16
parkervg <parkervg5@gmail.com>
2020-10-17 11:39:36 -0700

    Maven package call, changing port to 8081

--- bin/start-annis-frontend.sh
+++ bin/start-annis-frontend.sh
@@ -2,5 +2,9 @@
 source ./bin/config.sh
 
 echo Starting Tomcat...
+
+cd ${TOMCAT_DIR}/webapps/ROOT/META-INF/maven/de.hu-berlin.german.korpling.annis/annis-gui && \
+sudo mvn package
+
 cd ${TOMCAT_DIR}/webapps/ROOT/META-INF/maven/de.hu-berlin.german.korpling.annis/annis-gui && \
 sudo nohup mvn tomcat:run-war &
