3545b46a0570f0845175a7d0b8027baad6a92d07
parkervg <parkervg5@gmail.com>
2020-10-17 16:24:59 -0700

    Opening port 8080 if necessary

--- bin/start-annis-frontend.sh
+++ bin/start-annis-frontend.sh
@@ -3,15 +3,18 @@ source ./bin/config.sh
 
 echo Starting Tomcat...
 
-# # Check if java is running at port :8080
-# if [[ -n $(ps -ef | egrep '\-Dannis\.home\=annis\-service') ]]; then
-#   echo Killing Java at port 8080...
-#   annis_pid=$(ps -ef | egrep '\-Dannis\.home\=annis\-service' | awk '{print $2}')
-#   sudo kill -9 ${annis_pid}
-# fi
-#
-#
-#
+# Check if java is running at port :8080
+# Not really sure why this holds up tomcat sometimes
+# Maybe left from lingering process?
+# Look into ways other than mvn:tomcat shutdown to stop tomcat 
+if [[ -n $(lsof -i :8080 | egrep 'java') ]]; then
+  echo Killing Java at port 8080...
+  java_pid=$(lsof -i :8080 | egrep 'java' | awk '{print $2}' | tail -1)
+  sudo kill -9 ${java_pid}
+fi
+
+
+
 cd ${TOMCAT_DIR}/webapps/ROOT/META-INF/maven/de.hu-berlin.german.korpling.annis/annis-gui && \
 sudo mvn package
 
