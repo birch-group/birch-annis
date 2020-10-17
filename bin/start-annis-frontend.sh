053fade74436d629717866ccbcceb29554b37102
parkervg <parkervg5@gmail.com>
2020-10-16 22:33:19 -0700

    '&' after nohup call

--- bin/start-annis-frontend.sh
+++ bin/start-annis-frontend.sh
@@ -3,4 +3,4 @@ source ./bin/config.sh
 
 echo Starting Tomcat...
 cd ${TOMCAT_DIR}/webapps/ROOT/META-INF/maven/de.hu-berlin.german.korpling.annis/annis-gui && \
-sudo nohup mvn tomcat:run-war
+sudo nohup mvn tomcat:run-war &
