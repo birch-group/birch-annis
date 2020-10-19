61e28c25a3f8dd89be72cce78393383f86368682
parkervg <parkervg5@gmail.com>
2020-10-19 16:13:17 -0700

    tail -f on maven log

--- maven-log.sh
+++ maven-log.sh
@@ -2,4 +2,4 @@
 source ./bin/config.sh
 
 # Grab maven log from nohup.out
-tail -100 ./${ANNIS_GUI_DIR}/META-INF/maven/de.hu-berlin.german.korpling.annis/annis-gui/nohup.out
+tail -f ./${ANNIS_GUI_DIR}/META-INF/maven/de.hu-berlin.german.korpling.annis/annis-gui/nohup.out
