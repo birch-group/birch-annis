600206df9e8560015372c296b5655dbecfdf20ad
parkervg <parkervg5@gmail.com>
2020-10-19 11:17:27 -0700

    Unneeded comments

--- bin/configure-gui.sh
+++ bin/configure-gui.sh
@@ -2,21 +2,6 @@
 source ./bin/config.sh
 
 
-# if [ -z "$TOMCAT_DIR" ]; then
-# 	echo -e "${RED}Make sure TOMCAT_DIR is set as an environment variable${RESET}"
-# 	exit 2
-# fi
-
-
-# if  [[ -n $(cd ${TOMCAT_DIR}/webapps && ls | grep 'ROOT') ]]; then
-# 	echo Existing ROOT found, replacing it now...
-# 	sudo rm -r ${TOMCAT_DIR}/webapps/ROOT
-# fi
-
-# # Move to tomcat/webapps directory on VM
-# sudo cp -r ./${ANNIS_GUI_DIR} ${TOMCAT_DIR}/webapps
-#
-
 # Make directories needed for maven
 sudo mkdir ./${ANNIS_GUI_DIR}/META-INF/maven/de.hu-berlin.german.korpling.annis/annis-gui/src
 sudo mkdir ./${ANNIS_GUI_DIR}/META-INF/maven/de.hu-berlin.german.korpling.annis/annis-gui/src/main
